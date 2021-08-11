44ac29175c3d1bb84a7255c9d5c096e7366261e5
ruben_mx2 <rubengza.98@gmail.com>
2021-08-11 14:49:35 -0600

    change debug

--- DevStore/settings.py
+++ DevStore/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8lf^phdhs1xh5ga*_b+lh6gt6v$$)m#@hm(2lkj4u0ynwu+-ne'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
