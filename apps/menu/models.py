60f4a3d2af0bb47a78a34301c0a11e2cac538405
Gaby <madaydomtt@gmail.com>
2021-08-25 17:02:12 -0600

    change models

--- apps/menu/models.py
+++ apps/menu/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class restaurant(models.Model):
+    pk_restaurant = models.AutoField(primary_key=True, null=False, blank=False)
+    saucer = models.CharField(max_length=50, null=False, blank=False)
+    invoice = models.TextField(null=False, blank=False)
+    employee = models.CharField(max_length=50, null=False, blank=False)
+    client = models.CharField(max_length=50, null=False, blank=False)
+    direction = models.CharField(max_length=100, null=False, blank=False)
+
+class product(models.Model):
+    pk_product = models.AutoField(primary_key=True, null=False, blank=False)
+    code = models.IntegerField(null=False, blank=False)
+    saucer = models.CharField(max_length=50, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    price = models.DecimalField(null=False, blank=False, max_digits=3, decimal_places=2)
+    fk_restaurant = models.ForeignKey(restaurant, null=False, blank=False, on_delete=models.CASCADE)
+
